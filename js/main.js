a8c31b7545ce88e1a4874422e9ff6ec8cd08e0d5
vafafarzaliyeva <vafahf@code.edu.az>
2022-05-11 17:18:35 +0400

    responsive header , sidebar

--- js/main.js
+++ js/main.js
@@ -1,34 +1,78 @@
 $(document).ready(function(){
-  var modal = document.getElementById("myModal");
+  var modal1 = document.getElementById("myModal1");
 
 // Get the button that opens the modal
-var btn = document.getElementById("btnmod");
+var btn1 = document.getElementById("btnmod1");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn1.onclick = function() {
+  modal1.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span1.onclick = function() {
+  modal1.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
+
+
+
+
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("btnmod2");
+
+// Get the <span> element that closes the modal
+var span2 = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+  else if(event.target==modal2){
+    modal2.style.display="none";
   }
 }
 
-$("#allcatg").click(function(){
+
+
+
+$("#allcategory1").click(function(){
       $(".dropdown-category").toggle()
      
 })
 
+$("#allcategory2").click(function(){
+  $(".dropdown-category").toggle()
+ 
+})
+
+
+// SIDEBAR 
+
+let buttonsidebar=document.getElementById("sidebarbtn");
+let closesidebar=document.getElementById("closesidebar");
+buttonsidebar.onclick=function(){
+  document.getElementById("phonesidebar").style.width = "100%";
+}
+closesidebar.onclick=function(){
+  document.getElementById("phonesidebar").style.width = "0";
+}
 
 })
 
