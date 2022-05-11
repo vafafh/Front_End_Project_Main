80d15e41d36c22db5963c5cacda65542adcab9b6
vafafarzaliyeva <vafahf@code.edu.az>
2022-05-12 01:52:59 +0400

    responsive sections

--- js/main.js
+++ js/main.js
@@ -74,5 +74,45 @@ closesidebar.onclick=function(){
   document.getElementById("phonesidebar").style.width = "0";
 }
 
+// SLIDER
+
+$('.slider').slick({
+  // dots: true,
+  infinite: true,
+  speed: 300,
+  // autoplay:true,
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  arrows:true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 })
 
