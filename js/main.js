f0900f914d8bf5a67ee70ad417a8f08bbe8a4fd4
vafafarzaliyeva <vafahf@code.edu.az>
2022-05-14 13:32:23 +0400

    feat:footer

--- js/main.js
+++ js/main.js
@@ -114,5 +114,22 @@ $('.slider').slick({
   ]
 });
 
+
+
+// HEADER-PHONE
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header-sticky");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 })
 
