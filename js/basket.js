1afa3e2c69147b6cd957f8e2a695476f2661658c
vafafarzaliyeva <vafahf@code.edu.az>
2022-05-15 22:51:36 +0400

    feat:basket

--- js/basket.js
+++ js/basket.js
@@ -1,5 +1,6 @@
 let allAddButtons=document.querySelectorAll(".addToCartBtn");
 let basketCount=document.getElementById('basketCount');
+let table=document.getElementById('tbl-basket');
 
 
     if(!localStorage.getItem('basket')){
@@ -51,20 +52,43 @@ WriteBasketCount();
 
 
 function GetBasketElement(){
-
+    let count=1;
     if(localStorage.getItem('basket')){
        let basket=JSON.parse(localStorage.getItem('basket'));
       if(basket.length>0){
-        basket.forEach(product => {
+            table.classList.remove('d-none');
+            basket.forEach(product => {
             let name=product.name;
-            let count=product.count;
+            let prod_count=product.count;
             let price=product.price;
             let totalprice=product.count*product.price;
             let imgsrc=product.image; 
+
+               
+            let html=`<tr id='table-tr'>
+            <th scope="row">${count}</th>
+            <td scope="row"><img style="width: 20%;" src="${imgsrc}"  alt=""></td>
+            <td>${name.split(' ')[2]}</td>
+            <td>$${Number(price).toFixed(1)}</td></td>
+            <td class="td-quantity">
+            <span class="btn-minus"><i class="klbth-icon-minus"></i></span>
+            <span>${prod_count}</span>
+            <span class="btn-plus"><i class="klbth-icon-plus"></i></span>
+           
+            </td>
+            <td>$${Number(totalprice).toFixed(1)}</td>
+            <td><i class="klbth-icon-cancel"></i></td>
+          </tr>`
+         
+       
+            table.lastElementChild.innerHTML+=html;
+            count++;
+            
         }); 
       }
     }
     else{
+        
 
     }
 
@@ -72,6 +96,34 @@ function GetBasketElement(){
 
 GetBasketElement();
 
+function IncreaseDecrease(){
+    let minusbtns=document.querySelectorAll('.btn-minus')
+            let plusbtns=document.querySelectorAll('.btn-plus')
+
+            minusbtns.forEach(minusbtn => {
+                minusbtn.onclick=function(e){
+                    
+                    if(Number(e.target.nextElementSibling.innerText)>1){
+                          let product_count=Number(e.target.nextElementSibling.innerText)-1;
+                          e.target.nextElementSibling.innerText=product_count;
+                    }
+                    else{
+                        document.getElementById('table-tr').remove();
+                    }
+                  
+                }
+            });
+           
+           plusbtns.forEach(plusbtn=>{
+            plusbtn.onclick=function(e){
+                 console.log(e.target.previousElementSibling.innerText);
+                let product_count=Number(e.target.previousElementSibling.innerText)+1;
+                e.target.previousElementSibling.innerText=product_count;
+              }
+           })
+}
+
+IncreaseDecrease();
 
 
 // let price="$103457349853798";
